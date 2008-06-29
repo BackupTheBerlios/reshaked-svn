2e222d644e3d0a49269273894df21f98c0ea06a7
reduz <reduz@f29b5cec-baf9-0310-81f7-bdff227380de>
2008-06-29 05:29:32 +0000

    ..

---
+++ trunk/nodes/reverb_node.cpp
@@ -0,0 +1,23 @@
+//
+// C++ Implementation: reverb_node
+//
+// Description: 
+//
+//
+// Author: Juan Linietsky <reduzio@gmail.com>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "reverb_node.h"
+
+ReverbNode::ReverbNode()
+{
+}
+
+
+ReverbNode::~ReverbNode()
+{
+}
+
+
---
+++ trunk/nodes/reverb_node.h
@@ -0,0 +1,26 @@
+//
+// C++ Interface: reverb_node
+//
+// Description: 
+//
+//
+// Author: Juan Linietsky <reduzio@gmail.com>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef REVERB_NODE_H
+#define REVERB_NODE_H
+
+/**
+	@author Juan Linietsky <reduzio@gmail.com>
+*/
+class ReverbNode{
+public:
+    ReverbNode();
+
+    ~ReverbNode();
+
+};
+
+#endif
