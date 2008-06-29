28ad83469dbab344c8abc394cb1c317cb4b405d3
reduz <reduz@f29b5cec-baf9-0310-81f7-bdff227380de>
2008-06-29 06:24:12 +0000

    High Quality Reverb (will be called like this?)

---
+++ trunk/nodes/hq_reverb_node.cpp
@@ -0,0 +1,23 @@
+//
+// C++ Implementation: hq_reverb_node
+//
+// Description: 
+//
+//
+// Author: Juan Linietsky <reduzio@gmail.com>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "hq_reverb_node.h"
+
+HQ_ReverbNode::HQ_ReverbNode()
+{
+}
+
+
+HQ_ReverbNode::~HQ_ReverbNode()
+{
+}
+
+
---
+++ trunk/nodes/hq_reverb_node.h
@@ -0,0 +1,26 @@
+//
+// C++ Interface: hq_reverb_node
+//
+// Description: 
+//
+//
+// Author: Juan Linietsky <reduzio@gmail.com>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef HQ_REVERB_NODE_H
+#define HQ_REVERB_NODE_H
+
+/**
+	@author Juan Linietsky <reduzio@gmail.com>
+*/
+class HQ_ReverbNode{
+public:
+    HQ_ReverbNode();
+
+    ~HQ_ReverbNode();
+
+};
+
+#endif
